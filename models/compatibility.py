2b8b9ca548cf9d5c11ccef03cdf5bc7595aca749
elhussary <elhussary@protonmail.com>
2026-03-08 11:10:35 +0200

    feat: add sequence to view list

--- models/compatibility.py
+++ models/compatibility.py
@@ -4,9 +4,12 @@ from odoo import models, fields
 class ProductBrand(models.Model):
     _name = "product.brand"
     _description = "Product Brand"
+    _order = "sequence, id"
 
     name = fields.Char(string="Brand Name", required=True)
     reference = fields.Char(string="Brand Reference")
+    
+    sequence = fields.Integer(string="Sequence", default=10)
 
 
 class ProductConnection(models.Model):
